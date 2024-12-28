ac7a97eb2eb2e128c43675bdd6595d4bbe3cc940
MehulRakhecha06 <149859564+MehulRakhecha06@users.noreply.github.com>
2024-12-28 12:57:55 +0545

    added day 1 code

---
+++ day-1/main.py
@@ -0,0 +1,17 @@
+print("helo I am Ai bot.Whats your name?")
+
+name=input()
+
+print (f"nioce to meet you {name}")
+
+print("how are yoyu feeling today?(good/bad)")
+mood=input().lower()
+
+if mood=="good":
+    print("great to hear that")
+elif mood=="bad":
+    print("may god help you")
+else:
+    print("dimag ke upar se jara ha sab")
+
+print(f"it was nice meetin you{name}")
